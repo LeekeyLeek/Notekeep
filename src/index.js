b6abb370671dc821e035607c59a752a5fef73375
Malik Kilgore <malik199228@gmail.com>
2020-06-22 23:19:21 -0400

    Configured Webpack options and moved files.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 // This file should import Projects.js and contain DOM-related logic.
+import { project } from './modules/Projects'
+
+console.log(project.key1)
+console.log(project.key2)
--- src/modules/Projects.js
+++ src/modules/Projects.js
@@ -1,1 +1,7 @@
 //This will contain Todolists.js and will be used to have seperate lists of Todos. 
+const project = {
+    key1 : 'This is key 1',
+    key2 : 'This is key 2'
+}
+
+export { project }
